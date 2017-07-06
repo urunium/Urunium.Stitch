c6715ccd8e73939a85e99642d0dcf171d2b31c71
nripendra <nripendra.newa@gmail.com>
2017-07-06 18:40:45 +0545

    Added Typescript transformer

---
+++ src/Urunium.Stitch/ModuleTransformers/TypescriptModuleTransformer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urunium.Stitch.ModuleTransformers
+{
+    public class TypescriptModuleTransformer : IModuleTransformer
+    {
+        IFileSystem _fileSystem;
+        Microsoft.ClearScript.ScriptEngine _engine;
+        public TypescriptModuleTransformer(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+            _engine = new Microsoft.ClearScript.V8.V8ScriptEngine();
+
+            string tsc = ResourceReader.Read("typescriptservices.js");
+            string bootStrap = @"function transform(source, filename, compilerOptions) {
+    return ts.transpileModule(source, { fileName: 'test.tsx', compilerOptions: compilerOptions, reportDiagnostics: true });
+}";
+            _engine.Execute(tsc);
+            _engine.Execute(bootStrap);
+        }
+
+        public IEnumerable<string> Extensions => new[] { "ts", "tsx" };
+
+        public Module Transform(Module module)
+        {
+            string fullModulePath = module.FullPath;
+            string moduleId = module.ModuleId;
+            string content = module.TransformedContent ?? _fileSystem.File.ReadAllText(fullModulePath);
+            module.OriginalContent = module.OriginalContent ?? content;
+
+            dynamic compilerOptions = _engine.Evaluate("value = { 'jsx': ts.JsxEmit.React }");
+            dynamic transpileResult = _engine.Invoke("transform", content, _fileSystem.Path.GetFileName(fullModulePath), compilerOptions);
+            
+            module.TransformedContent = transpileResult.outputText;
+            return module;
+        }
+    }
+}
---
+++ tests/Urunium.Stitch.Tests/TypescriptModuleTransformerTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Urunium.Stitch.ModuleTransformers;
+
+namespace Urunium.Stitch.Tests
+{
+    [TestFixture]
+    public class TypescriptModuleTransformerTests
+    {
+        [Test]
+        public void TransFormTest()
+        {
+            // Arrange
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { @"c:\App\App.tsx", new MockFileData("export class Component extends React.Component<TProp, TState> { render() { return <div>Hello world</div>;}}") },
+                { @"c:\App\font.ttf", new MockFileData("ttffilehere") }
+            });
+
+            var packager = new Packager(
+                            fileSystem: fileSystem,
+                            handlers: new IModuleTransformer[]
+                            {
+                                new TypescriptModuleTransformer(fileSystem)
+                            });
+            Package package = packager.Package(new SourceConfig
+            {
+                RootPath = @"c:\App",
+                EntryPoints = new[] { "./App" },
+                Globals = new GlobalsConfig
+                {
+                    ["react"] = "React",
+                    ["react-dom"] = "ReactDOM"
+                },
+                CopyFiles = new[] { "./font.ttf" }
+            });
+            Assert.AreEqual(3, package.Modules.Count);
+            Assert.AreEqual(1, package.Files.Count);
+
+            var transpiled = @"""use strict"";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var Component = (function (_super) {
+    __extends(Component, _super);
+    function Component() {
+        return _super.apply(this, arguments) || this;
+    }
+    Component.prototype.render = function () { return React.createElement(""div"", null, ""Hello world""); };
+    return Component;
+}(React.Component));
+exports.Component = Component;".Replace("\r\n", "");
+            Assert.AreEqual(transpiled, package.Modules[2].TransformedContent.Replace("\r\n", ""));
+        }
+    }
+}
